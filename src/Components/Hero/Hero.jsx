c9dc022baf607fe460cac2fbe65d7b0fc8741737
Sumit_Lohar <loharsumit693@gmail.com>
2024-08-19 20:47:40 +0530

    .

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -7,11 +7,11 @@ const Hero = () => {
   return (
     <div id='home' className='hero'>
         <img src={profile_img} alt="" />
-        <h1><span>I'm Sumit Lohar</span>, frontend developer based in India.</h1>
+        <h1><span>I'm Sumit Lohar</span>, frontend developer.</h1>
         <p>Aspiring Web Developer | HTML, CSS, JavaScript, React.js | Passionate About Creating Responsive & User-Friendly Websites</p>
         <div className="hero-action">
             <div className="hero-connect"><AnchorLink className='anchor-link' offset={50} href='#contact'>Connect with me</AnchorLink></div>
-            <div className="hero-resume">My resume</div>
+            <div className="hero-resume"><a href='https://sumitloharresume.tiiny.site/'>My resume</a></div>
         </div>
     </div>
   )
