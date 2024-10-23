189877a71af877661c088aa598220268b3274ae1
Sumit_Lohar <loharsumit693@gmail.com>
2024-10-23 12:28:03 +0530

    updated projects

--- src/assets/mywork_data.js
+++ src/assets/mywork_data.js
@@ -1,40 +1,46 @@
-import project1_img from '../assets/project_1.svg'
-import project2_img from '../assets/project_2.svg'
-import project3_img from '../assets/project_3.svg'
-import project4_img from '../assets/project_4.svg'
-import project5_img from '../assets/project_5.svg'
-import project6_img from '../assets/project_6.svg'
+import project1_img from '../assets/project_1.png'
+import project2_img from '../assets/project_2.png'
+import project3_img from '../assets/project_3.png'
+import project4_img from '../assets/project_4.png'
+import project5_img from '../assets/project_5.png'
+import project6_img from '../assets/project_6.png'
 
 const mywork_data = [
     {
         w_no:1,
         w_name:"Web design",
-        w_img:project1_img
+        w_img:project1_img,
+        w_link: "https://responsive-bakery-website.vercel.app/"
     },
     {
         w_no:2,
         w_name:"Web design",
-        w_img:project2_img
+        w_img:project2_img,
+        w_link: "https://lenskart-clone-lilac.vercel.app/"
     },
     {
         w_no:3,
         w_name:"Web design",
-        w_img:project3_img
+        w_img:project3_img,
+        w_link: "https://travel-agency-website-beta.vercel.app/"
     },
     {
         w_no:4,
         w_name:"Web design",
-        w_img:project4_img
+        w_img:project4_img,
+        w_link: "https://hair-salon-website-nine.vercel.app/"
     },
     {
         w_no:5,
         w_name:"Web design",
-        w_img:project5_img
+        w_img:project5_img,
+        w_link: "https://flipkart-clone-nine-bay.vercel.app/"
     },
     {
         w_no:6,
         w_name:"Web design",
-        w_img:project6_img
+        w_img:project6_img,
+        w_link: "https://weather-app-lovat-six-89.vercel.app/"
     },
 ]
  
