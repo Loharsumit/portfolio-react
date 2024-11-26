a84e5f44a355101e7ea7f1ba6bde4ae3ea42cad1
Sumit_Lohar <loharsumit693@gmail.com>
2024-11-26 18:08:23 +0530

    .

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './Hero.css'
 import profile_img from '../../assets/profile_img.png'
 
+
 const Hero = () => {
   return (
     <div id='home' className='hero'>
@@ -10,7 +11,7 @@ const Hero = () => {
         <p>Aspiring Web Developer | HTML, CSS, JavaScript, React.js | Passionate About Creating Responsive & User-Friendly Websites</p>
         <div className="hero-action">
             <a className="hero-connect" href='https://www.linkedin.com/in/sumit-lohar-6844a917a/' offset={50}>Connect me on LinkedIn</a>
-            <a href='https://loharsumit-cv.tiiny.site' className="hero-resume">My Resume</a>
+            <a href='https://drive.google.com/file/d/1j7Eo2jaNruTFB7Pox6iBqqiFDwUKjZsy/view?usp=sharing' className="hero-resume">My Resume</a>
         </div>
     </div>
   )
