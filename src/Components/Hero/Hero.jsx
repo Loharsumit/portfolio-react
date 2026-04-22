c58c3391b90b5c89da31c51aade9403eaf1f420c
SUMIT LOHAR <94884266+Loharsumit@users.noreply.github.com>
2026-04-22 20:21:35 +0530

    Revise Hero component text and resume link
    Updated the hero section to reflect current skills and experience.

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -7,11 +7,11 @@ const Hero = () => {
   return (
     <div id='home' className='hero'>
         <img src={profile_img} alt="" />
-        <h1><span>I'm Sumit Lohar</span>, frontend developer.</h1>
-        <p>Aspiring Web Developer | HTML, CSS, JavaScript, React.js | Passionate About Creating Responsive & User-Friendly Websites</p>
+        <h1><span>I'm Sumit Lohar</span>, Web Developer.</h1>
+        <p>Experienced Web Developer | Proficient in HTML, CSS, JavaScript and React.js | Vue.js | 3+ Yrs | Bengaluru | Passionate about Creating Innovative and User-Friendly Websites.</p>
         <div className="hero-action">
             <a className="hero-connect" href='https://www.linkedin.com/in/sumit-lohar-6844a917a/' target="_blank" offset={50}>Connect me on LinkedIn</a>
-            <a href='https://drive.google.com/file/d/1gudVyem7eseodCqG60p07OgFNsNpEzuR/view?usp=sharing' target="_blank" className="hero-resume">My Resume</a>
+            <a href='https://drive.google.com/file/d/1IJAgPNShKPzKVISAhTfT-AY6TQayi4qq/view?usp=sharing' target="_blank" className="hero-resume">My Resume</a>
         </div>
     </div>
   )
